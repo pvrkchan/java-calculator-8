cbc8c9ebdc0a98ff89651f45024ad8121e51e801
pvrkchan <pvrkchan@naver.com>
2025-10-18 00:34:40 +0900

    refactor : validate가 view 내에서 진행되도록 리팩터링

--- src/main/java/calculator/Utility.java
+++ src/main/java/calculator/Utility.java
@@ -5,12 +5,8 @@ import java.util.regex.Pattern;
 public class Utility {
     public static String findCustomSeperator(String str) {
         String seperatorRegex = ",|:";
-        if(str.startsWith("//")){
-            int index = str.indexOf("\\n");
-            if(index!=3)
-                throw new IllegalArgumentException("입력이 잘못되었습니다.");
+        if(str.startsWith("//"))
             seperatorRegex += "|" + Pattern.quote(str.substring(2,3));
-        }
         return seperatorRegex;
     }
 
--- src/main/java/calculator/view/inputView.java
+++ src/main/java/calculator/view/inputView.java
@@ -6,7 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class inputView {
     public static void inputString() {
         String str = Console.readLine();
+        validateCustomSeperator(str);
         String seperatorRegex = Utility.findCustomSeperator(str);
         str = Utility.makeTargetString(str);
+    }
+
+    public static void validateCustomSeperator(String str){
+        if(str.startsWith("//")){
+            int index = str.indexOf("\\n");
+            if(index!=3)
+                throw new IllegalArgumentException("입력이 잘못되었습니다.");
+        }
     }
 }
