fe9e35dc8ec4836c192c172ef3055e52fad66502
pvrkchan <pvrkchan@naver.com>
2025-10-17 23:35:57 +0900

    feat : 사용자로부터 문자열 입력 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.view.inputView;
+
 public class Controller {
     public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
+        inputView.inputString();
     }
 }
---
+++ src/main/java/calculator/view/inputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class inputView {
+    public static void inputString() {
+        String str = Console.readLine();
+    }
+}
