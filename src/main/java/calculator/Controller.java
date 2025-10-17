ed6d2de4f0d7c6eb2d17026f2fee42a403378d50
pvrkchan <pvrkchan@naver.com>
2025-10-18 01:23:49 +0900

    feat : 숫자의 합 출력 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -2,11 +2,12 @@ package calculator;
 
 import calculator.model.Calculator;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Controller {
     public void run() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Calculator calculator = new Calculator(InputView.inputString());
-        calculator.calculate();
+        OutputView.printResult(calculator.calculate());
     }
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,7 @@
+package calculator.view;
+
+public class OutputView {
+    public static void printResult(long result) {
+        System.out.println("결과 : " + result);
+    }
+}
