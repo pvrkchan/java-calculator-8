fc08fdc68fb04960cf3917d31d6b420b54471e5a
pvrkchan <pvrkchan@naver.com>
2025-10-18 18:09:18 +0900

    test : 검증 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,18 +8,59 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 커스텀_구분자_사용() {
+    void 빈_문자열입력() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
         });
     }
 
     @Test
-    void 예외_테스트() {
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2,3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    void 일반_구분자_사용_여러숫자() {
+        assertSimpleTest(() -> {
+            run("123,5,6:7");
+            assertThat(output()).contains("결과 : 141");
+        });
+    }
+
+    @Test
+    void 예외_커스텀_구분자_2글자() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//*^\\n1,23"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_음수() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_제로() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_등록되지_않은_구분자(){
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("//*\\n1,2.3"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
