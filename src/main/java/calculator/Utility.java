f4020a45c02ce7d2b98d765e1bb046fe9b41cd64
pvrkchan <pvrkchan@naver.com>
2025-10-19 15:56:03 +0900

    refactor : 커스텀 구분자 개수 판단 함수화

--- src/main/java/calculator/Utility.java
+++ src/main/java/calculator/Utility.java
@@ -5,19 +5,27 @@ import java.util.regex.Pattern;
 public class Utility {
     public static String findCustomSeperator(String str) {
         String seperatorRegex = ",|:";
-        if(str.startsWith("//") && str.indexOf("\\n")==3) {
+        if(hasOneCustomSeperator(str)) {
             seperatorRegex += "|" + Pattern.quote(str.substring(2, 3));
         }
         return seperatorRegex;
     }
 
     public static String makeTargetString(String str) {
-        if(str.startsWith("//") && str.indexOf("\\n")==3){
+        if(hasOneCustomSeperator(str)){
             return str.substring(5);
-        } else if(str.startsWith("//") && str.indexOf("\\n")==2){
+        } else if(hasNoCustomSeperator(str)){
             return str.substring(4);
         } else {
             return str;
         }
+    }
+
+    public static boolean hasOneCustomSeperator(String str) {
+        return str.startsWith("//") && str.indexOf("\\n")==3;
+    }
+
+    public static boolean hasNoCustomSeperator(String str) {
+        return str.startsWith("//") && str.indexOf("\\n")==2;
     }
 }
